644f6623130c7ba778c6a8768b3e30eb8e684d2f
rosendo <rosendc30@gmail.com>
2024-09-24 23:17:21 -0300

    correção hooks

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -5,12 +5,12 @@ import { api } from "../services/api"
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+    const [data, setData] = useState({})
     async function signIn({ email, password }) {
-        const [data, setData] = useState({})
         try {
             const response = await api.post("/sessions", { email, password })
             const { user, token } = response.data
-            
+
             api.defaults.headers.authorization = `Bearer ${token}`
             setData({ user, token })
         } catch (error) {
